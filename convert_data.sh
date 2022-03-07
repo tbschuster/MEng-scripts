ca7f6321d764882bbcaddfa8543145bd5d80c9e6
Beatrice Schuster <tbschuster1@gmail.com>
2022-03-07 20:57:57 +0100

    convert_data.sh: updated version, permutes lines in the .bvec file

--- convert_data.sh
+++ convert_data.sh
@@ -4,6 +4,15 @@
 set -e
 mkdir -p $1
 
+bvec="Original/b2000-reg-split/b2000-AP-neg.bvec"
+
+while IFS= read -r line || [ -n "$line" ]; do
+    a+=("$line")
+done < $bvec
+
+echo -e ${a[1]}"\n"${a[2]}"\n"${a[0]} > bvec_corrected.bvec
+
+
 for f in Original/*; do
 
 in=$f/*-$1
