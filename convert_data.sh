bee7d5a8e9c75c6d20c975024a948de80ace030d
Beatrice Schuster <tbschuster1@gmail.com>
2022-03-07 23:06:24 +0100

    convert_data.sh: update: successfully uses the permuted .bvec files before data conversion

--- convert_data.sh
+++ convert_data.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-#To use, example: ./convert_data.sh AP-neg
+#Usage example: ./convert_data.sh AP-neg
 
 set -e
 mkdir -p $1
 
-bvec="Original/b2000-reg-split/b2000-AP-neg.bvec"
-
-while IFS= read -r line || [ -n "$line" ]; do
-    a+=("$line")
-done < $bvec
-
-echo -e ${a[1]}"\n"${a[2]}"\n"${a[0]} > bvec_corrected.bvec
 
 
 for f in Original/*; do
@@ -18,9 +11,20 @@ for f in Original/*; do
 in=$f/*-$1
 out=$(basename $f).mif
 
-mrconvert $in.nii -fslgrad $in.bvec $in.bval $1/$out -force
+bvec=$f/*-$1.bvec
+
+a=()
+while IFS= read -r line || [ -n "$line" ]; do
+    a+=("$line")
+done < $bvec
+
+echo -e ${a[1]}"\n"${a[2]}"\n"${a[0]} > $f/bvec_corrected.bvec
+
+mrconvert $in.nii -fslgrad $f/bvec_corrected.bvec $in.bval $1/$out -force
 
 done
+
+
 
 mrcat $1/b*-reg-split.mif -axis 3 $1/dwi_shells.mif -force
 rm $1/b*-reg-split.mif
