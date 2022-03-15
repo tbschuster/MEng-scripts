4c6049dd72d69b54dbedba811cc800f0d9a31c32
Beatrice Schuster <tbschuster1@gmail.com>
2022-03-15 14:24:11 +0100

    conv_noise.sh: working script that generates fake DW encoding with bvals from 0 to 10000

--- conv_noise.sh
+++ conv_noise.sh
@@ -10,11 +10,7 @@ sh2amp sh.mif dwi_shells.mif dwi_predicted_orig.mif -force
 
 mrcalc dwi_predicted_orig.mif randn 50 -mult -add dwi_noisy_orig.mif -force
 
-for b in $(seq 0 200 10000); do
+for b in $(seq 0 200 10000); do dirgen 12 -cartesian tmp.txt -quiet -force; awk '{ if ($1 != "#") print $0, '$b'; }' < tmp.txt; done > grad.txt
 
-dirgen 12 -cartesian tmp.txt; -quiet -force
-awk ‘{ if ($1 != "#") print $0, "$b"; }’ < tmp.txt
-
-done > grad.txt
 
 rm sh.mif
