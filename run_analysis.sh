48bf12f829a53c2c998ebb68512872785108434e
Beatrice Schuster <tbschuster1@gmail.com>
2022-03-22 04:23:25 +0100

    run_analysis.sh: added msmt command to noisy data

--- run_analysis.sh
+++ run_analysis.sh
@@ -9,15 +9,16 @@ set -e
 # Generate responses using MATLAB, instructions in response_documentation.m
 
 for folder in AP-neg; do
-#./convert_data.sh $folder
+./convert_data.sh $folder
 (
 cd $folder
-#../get_response.sh
-#../dwi2fod.sh
+../get_response.sh
+../dwi2fod.sh
 ../simulate.sh ../grad.txt dwi_sim.mif
 
 ../sample_selection.sh dwi_sim.mif 100 dwi_sim_subsample.mif
 ../add_noise.sh dwi_sim_subsample.mif 50 dwi_sim_subsample_noise.mif
 
+~/Documents/mrtrix3-tbschuster/bin/dwi2fod msmt_csd dwi_sim_subsample_noise.mif wm_response.txt wm_sim.mif gm_response.txt gm_sim.mif csf_response.txt csf_sim.mif -mask mask.mif -force
 )
 done
